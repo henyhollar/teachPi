dc6ceaba37c97f4f666a63b93835e639aa06d4fe
henyhollar <henyhollar@gmail.com>
2015-12-05 11:36:18 +0100

    cors

--- student/serializers.py
+++ student/serializers.py
@@ -51,32 +51,37 @@ class RegisterSerializer(serializers.Serializer):
         #style = {'input-type': 'password', 'placeholder': 'Confirm Password'}
     #)
 
-	
-    
+	def validate_username(self, value):
+		try:
+			user = User.objects.get(username=value)
+			raise serializers.ValidationError('Username already exists please choose another')
+		except User.DoesNotExist:
+			return value
+			
 	def validate_email(self, value):
 		try:
 			user = User.objects.get(email=value)
-			if user.username == self.instance.username:
-				return value
-			else:
-				raise serializers.ValidationError('Email already exists please choose another')
+			raise serializers.ValidationError('Email already exists please choose another')
 		except User.DoesNotExist:
 			return value
-		#except MultipleObjectsReturned:
-            ##if value != u'':
-			#raise serializers.ValidationError('Please enter an email address')
+		
 			
 			
-	#def validate_matric_no(self, value):
-		#matric_no = value.upper()
-        #try:
-            #user = User.objects.get(matric_no=matric_no)
-        #except User.DoesNotExist:
-			##check_matric_no(matric_no)
-			#return val
+	def validate_matric_no(self, value):
+		matric_no = value.upper()
+		
+		if any([all([matric_no.startswith('EEG'),len(value) == 12]), 
+			all([matric_no.startswith('TP'),len(value) == 14]),
+			all([matric_no.startswith('AC'),len(value) == 8])]):
+			try:
+				user = User.objects.get(matric_no=value)
+				raise serializers.ValidationError('Matric No. already exists please report if someone else has used yours')
+			except User.DoesNotExist:
+				return value
+		else:
+			raise serializers.ValidationError('Matric No. is not valid')
 
-        #raise serializers.ValidationError('Please check the matric_no')
-
+			
 
 	#def validate(self, data):
 		#if data['password'] == data['confirm_password']:
