761e9f822daab8c5a73edccc21776f94f0773b4c
henyhollar <henyhollar@gmail.com>
2015-12-24 01:52:49 +0100

    cantakecourse

--- course/urls.py
+++ course/urls.py
@@ -4,7 +4,8 @@ from .import views
 
 
 urlpatterns = [
-	url(r'^$', views.CourseView.as_view(), name='post_course'),
-	url(r'^(?P<course_code>[A-Z0-9]{6})/$', views.CourseView.as_view(), name='get_course'),
-	url(r'^can_take_course/$', views.CanTakeCourse.as_view(), name='can_take_course'),
+	url(r'^$', views.CourseView.as_view(), name='course'),
+	url(r'^can_take_course/$', views.CanTakeCourse.as_view(), name='post_can_take_course'),
+	url(r'^can_take_course/(?P<course_code>[A-Z0-9]{6})/$', views.CanTakeCourse.as_view(), name='get_can_take_course'),
+
 ]
