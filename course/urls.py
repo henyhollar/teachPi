b452c4187da07e5ef7385b8d0e53301da4529d52
Heny Hollar <henyhollar@gmail.com>
2016-01-11 22:41:40 +0100

    time left

--- course/urls.py
+++ course/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 	url(r'^$', views.CourseView.as_view(), name='course'),
 	url(r'^can_take_course/$', views.CanTakeCourse.as_view(), name='post_can_take_course'),
 	url(r'^can_take_course/(?P<course_code>[A-Z0-9]{6})/$', views.CanTakeCourse.as_view(), name='get_can_take_course'),
-	url(r'^time_left/$', views.get_time_left(), name='get_time_left'),
+	url(r'^time_left/$', views.get_time_left, name='get_time_left'),
 
 ]
